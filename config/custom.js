16b084d024c09a45080463626e75fcaf1c9e8f63
oso <jose.carlos9123@gmail.com>
2020-10-06 15:42:57 -0500

    remove keys

--- config/custom.js
+++ config/custom.js
@@ -15,11 +15,6 @@ module.exports.custom = {
   * Any other custom config this Sails app should use during development.    *
   *                                                                          *
   ***************************************************************************/
-  mailgunDomain: 'sandbox26bff8b0425440a2a915e6338f805dac.mailgun.org',
-  mailgunSecret: 'key-76nquidh4oxftx12e3pqwz4mdkk6p-u1',
-  mailgunPwd:'bc801dbf1875f7a59e87038837ced363-aff2d1b9-f96960ae',
-  mailgunUsername: 'postmaster@sandbox26bff8b0425440a2a915e6338f805dac.mailgun.org',
-  mainEmail:"soporte@test.com"
   // stripeSecret: 'sk_test_Zzd814nldl91104qor5911gjald',
   // …
 
